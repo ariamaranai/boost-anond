16b5098d190f679eb8c252f068f533959f0965d5
a <106607480+ariamaranai@users.noreply.github.com>
2025-05-30 08:34:43 +0900

    Add files via upload

---
+++ bg.js
@@ -0,0 +1,18 @@
+chrome.runtime.onInstalled.addListener(() => (
+  chrome.contentSettings.javascript.get({
+    primaryUrl: "https://anond.hatelabo.jp"
+  }, details =>
+    details.setting == "allow" && chrome.contentSettings.javascript.set({
+      primaryPattern: "https://anond.hatelabo.jp/*",
+      setting: "block"
+    })
+  ),
+  chrome.contentSettings.images.get({
+    primaryUrl: "https://anond.hatelabo.jp"
+  }, details =>
+    details.setting == "allow" && chrome.contentSettings.images.set({
+      primaryPattern: "https://anond.hatelabo.jp/*",
+      setting: "block"
+    })
+  )
+));
